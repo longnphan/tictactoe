9778468bdfae56d662dee3a86fa229a57f457dea
longnphan <67768035+longnphan@users.noreply.github.com>
2020-08-16 11:06:00 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const tile7 = document.querySelector('.seven');
 const tile8 = document.querySelector('.eight');
 const tile9 = document.querySelector('.nine');
 
+
 let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 let count = 0;
 let tie = true;
@@ -25,76 +26,77 @@ function markTile() {
   player1 === true ? player1 = false : player1 = true;
   tie = true;
 
-  if (arr[0]  == arr[1] && arr[0] == arr[2]) {
+  if (arr[0]  === arr[1] && arr[0] === arr[2]) {
     tile1.style.backgroundColor = 'yellow';
     tile2.style.backgroundColor = 'yellow';
     tile3.style.backgroundColor = 'yellow';
     tie = false;
     setTimeout(function() {alert(`"${arr[0]}"  Won!!!`);
-    reset();}, 200)
+    reset();}, 100)
     // alert(`"${arr[0]}"  Won!!!`);
     // reset();
-  } else if (arr[3] == arr[4] && arr[3] == arr[5]) {
+  } else if (arr[3] === arr[4] && arr[3] === arr[5]) {
     tile4.style.backgroundColor = 'yellow';
     tile5.style.backgroundColor = 'yellow';
     tile6.style.backgroundColor = 'yellow';
     tie = false;
     setTimeout(function() {alert(`"${arr[3]}"  Won!!!`);
-    reset();}, 200)
+    reset();}, 100)
 
-  } else if (arr[6] == arr[7] && arr[6] == arr[8]) {
+  } else if (arr[6] === arr[7] && arr[6] === arr[8]) {
     tile7.style.backgroundColor = 'yellow';
     tile8.style.backgroundColor = 'yellow';
     tile9.style.backgroundColor = 'yellow';
     tie = false;
     setTimeout(function() {alert(`"${arr[6]}"  Won!!!`);
-    reset();}, 200)
+    reset();}, 100)
 
-  } else if (arr[0] == arr[4] && arr[0] == arr[8]) {
+  } else if (arr[0] === arr[4] && arr[0] === arr[8]) {
     tile1.style.backgroundColor = 'yellow';
     tile5.style.backgroundColor = 'yellow';
     tile9.style.backgroundColor = 'yellow';
     tie = false;
     setTimeout(function() {alert(`"${arr[0]}"  Won!!!`);
-    reset();}, 200)
+    reset();}, 100)
 
-  } else if (arr[2] == arr[4] && arr[2] == arr[6]) {
+  } else if (arr[2] === arr[4] && arr[2] === arr[6]) {
     tile3.style.backgroundColor = 'yellow';
     tile5.style.backgroundColor = 'yellow';
     tile7.style.backgroundColor = 'yellow';
     tie = false;
     setTimeout(function() {alert(`"${arr[2]}"  Won!!!`);
-    reset();}, 200)
+    reset();}, 100)
 
-  } else if (arr[0] == arr[3] && arr[0] == arr[6]) {
+  } else if (arr[0] === arr[3] && arr[0] === arr[6]) {
     tile1.style.backgroundColor = 'yellow';
     tile4.style.backgroundColor = 'yellow';
     tile7.style.backgroundColor = 'yellow';
     tie = false;
     setTimeout(function() {alert(`"${arr[0]}"  Won!!!`);
-    reset();}, 200)
+    reset();}, 100)
 
-     } else if (arr[2] == arr[5] && arr[2] == arr[8]) {
+  } else if (arr[2] === arr[5] && arr[2] === arr[8]) {
     tile3.style.backgroundColor = 'yellow';
     tile6.style.backgroundColor = 'yellow';
     tile9.style.backgroundColor = 'yellow';
     tie = false;
     setTimeout(function() {alert(`"${arr[2]}"  Won!!!`);
-    reset();}, 200)
+    reset();}, 100)
 
-    } else if (arr[1] == arr[4] && arr[1] == arr[7]) {
+  } else if (arr[1] === arr[4] && arr[1] === arr[7]) {
     tile2.style.backgroundColor = 'yellow';
     tile5.style.backgroundColor = 'yellow';
     tile8.style.backgroundColor = 'yellow';
     tie = false;
     setTimeout(function() {alert(`"${arr[1]}"  Won!!!`);
-    reset();}, 200)
+    reset();}, 100)
      }
-  count++;
 
-  if(count === 9 && tie == true) {
-    alert('Tie');
-    reset();
+  count++;
+  if(count === 9 && tie === true) {
+    setTimeout(function() {alert('Tie');
+    reset();}, 100)
+
   }
 }
 
